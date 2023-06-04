9345dd7aca6796b6f3b61ca1786511f6faacdc41
byeongseok <kbs970318@gmail.com>
2023-06-04 12:54:23 +0900

    컨트롤러 서블릿/MemberVO,DAO/회원정보출력창 작성
    +DB 연결 (DBCP)

---
+++ MVC/src/main/java/sec01/ex01/MemberController.java
@@ -0,0 +1,44 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/mem.do")
+public class MemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	MemberDAO memberDAO;
+	
+	public void init() throws ServletException {
+	memberDAO= new MemberDAO(); //MemberDAO 생성	
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doHandle(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("/text/html;charset=utf-8");
+		List<MemberVO> membersList = memberDAO.listMembers();  //요청에 대해 회원 정보를 조회
+		request.setAttribute("membersList", membersList);  //조회한 회원 정보를 request에 바인딩
+		RequestDispatcher dispatch = request.getRequestDispatcher("/test01/listMembers.jsp");
+		dispatch.forward(request, response);  //컨트롤러에서 표시하고자 하는 JSP로 포워딩
+	}
+
+}
---
+++ MVC/src/main/java/sec01/ex01/MemberDAO.java
@@ -0,0 +1,81 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	
+	private DataSource dataFactory;
+	private Connection conn;
+	private PreparedStatement pstmt;
+
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/test");
+		} catch (Exception e) {
+			
+			
+			e.printStackTrace();
+		}
+
+	}
+
+	public List<MemberVO> listMembers() {   //listMembers 메서드 호출
+		List<MemberVO> membersList = new ArrayList<MemberVO>();
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from  t_member order by joinDate desc"; //SQL문 작성 : joindate
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);  //prepareStatement 객체를 생성하면서 SQL문을 인자로 전달
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO memberVO = new MemberVO(id, pwd, name, email, joinDate);
+				membersList.add(memberVO);  //memberList에 MemberVO 객체들을 차례대로 저장
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return membersList;  //회원 정보를 조회해서 membersList에 저장후 반환
+	}
+
+	public void addMember(MemberVO m) {  //회원 정보 추가 기능을 하는 addMember 메서드 추가
+		try {
+			conn = dataFactory.getConnection();
+			String id = m.getId();
+			String pwd = m.getPwd();
+			String name = m.getName();
+			String email = m.getEmail();
+			String query = "INSERT INTO t_member(id, pwd, name, email)" + " VALUES(?, ?, ?, ?)";   //SQL문 작성 : t_member 내용 추가
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query); //PrepareStatement 객체 생성하면서 SQL문을 인자로 전달
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, name);
+			pstmt.setString(4, email);
+			pstmt.executeUpdate();       //SQL문 실행
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
