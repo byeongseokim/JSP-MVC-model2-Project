5885e0739618c9b465ff877e0a6b7fb33660cf73
byeongseokim <kbs970318@gmil.com>
2023-06-05 17:46:53 +0900

    회원 정보 추가 기능 구현

---
+++ MVC/src/main/java/sec02/ex01/MemberController.java
@@ -0,0 +1,67 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/*")
+public class MemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	MemberDAO memberDAO;
+
+	public void init() throws ServletException {
+		memberDAO = new MemberDAO(); // MemberDAO 생성
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nextPage = null;
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("/text/html;charset=utf-8");
+		String action = request.getPathInfo();
+		System.out.println("action:" + action);
+
+		if (action == null || action.equals("/listMembers.do")) 
+		{
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			nextPage = "/test02/listMembers.jsp";
+		} else if (action.equals("/addMember.do")) {
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			MemberVO memberVO = new MemberVO(id, pwd, name, email);
+			memberDAO.addMember(memberVO);
+			nextPage = "/member/listMembers.do";
+		} else if (action.equals("/memberForm.do")) {
+			nextPage = "/test02/memberForm.jsp";
+		} else {
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			nextPage = "/test02/listMembers.jsp";
+
+		}
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		dispatch.forward(request, response); // 컨트롤러에서 표시하고자 하는 JSP로 포워딩
+	}
+
+}
